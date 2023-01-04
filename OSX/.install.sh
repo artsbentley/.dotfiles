912de8a88b5b7410d6cc002cd2c794dbbbf8aed5
Arno Arts <arar@Arnos-MacBook-Pro.local>
2023-01-04 12:29:48 +0100

    OSX setup scripts

---
+++ OSX/.install.sh
@@ -0,0 +1,95 @@
+#!/bin/zsh
+
+# Install Brew
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+
+brew tap homebrew/cask-fonts
+brew tap koekeishiya/formulae
+brew tap FelixKratz/formulae
+
+
+#brew formulae
+echo "Installing Brew Formulae..."
+brew install neovim
+brew install starship
+brew install alfred
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install yabai
+brew install skhd
+brew install sketchybar
+brew install sf-symbols
+brew install llvm
+brew install boost
+brew install mas
+brew install tree
+brew install wget
+brew install jq
+brew install lulu
+brew install lf
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+
+#brew install svim --gives vim commands to mac input fields
+
+# Brew Casks
+echo "Installing Brew Casks..."
+
+brew install --cask spotify
+brew install --cask vlc
+
+# Mac App Store Apps
+echo "Installing Mac App Store Apps..."
+#example:
+#mas install 1451685025 #Wireguard
+
+# macOS Settings
+echo "Changing macOS defaults..."
+defaults write com.apple.dock autohide -bool true
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
+defaults write NSGlobalDomain AppleHighlightColor -string "0.65098 0.85490 0.58431"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder ShowStatusBar -bool false
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Installing Fonts
+echo "Installing fonts..."
+git clone https://github.com/shaunsingh/SFMono-Nerd-Font-Ligaturized /tmp/SFMono_Nerd_Font
+mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
+rm -rf /tmp/SFMono_Nerd_Font/
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+
+# Python Packages
+echo "Installing Python Packages..."
+#pip install ....
+
+
+# Start Services
+echo "Starting Services (grant permissions)..."
+brew services start skhd
+brew services start yabai
+brew services start sketchybar
+#brew services start svim
+
+
+
+
+echo "Installation complete...\nRun nvim +PackerSync and Restart..."
