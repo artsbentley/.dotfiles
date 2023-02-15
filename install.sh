f8ffd725c145edf354f0bb0f0467e95d74285079
Arno Arts <arar@arnos-mbp.home>
2023-02-15 13:01:12 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing dotfiles"
+
+" Check that all of our submodules work has been published before pushing the superproject
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+source install/link.sh
