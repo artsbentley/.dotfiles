8971b5938a90abb712dbc9ff8e1260771dbd0a6b
Arno Arts <arar@arnos-mbp.home>
2023-02-22 00:16:09 +0100

    catppuccin comments color - italic not working

--- .config/nvim/lua/plugins/catppuchin.lua
+++ .config/nvim/lua/plugins/catppuchin.lua
@@ -1,26 +1,37 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  event = "VeryLazy",
-  opts = {
-    flavour = "mocha", -- latte, frappe, macchiato, or mocha
-    transparent_background = true,
-    color_overrides = {
-      all = {
-        surface0 = "#444444",
-        surface1 = "#666666",
-        surface2 = "#a3a7bc",
-        surface3 = "#a3a7bc",
-      },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    event = "VeryLazy",
+    opts = {
+        styles = {
+            comments = { "italic" },
+            conditionals = { "italic" },
+            loops = {},
+            functions = { "bold" },
+            keywords = {},
+            strings = {},
+            variables = {},
+            numbers = {},
+            booleans = {},
+            properties = {},
+            types = {},
+            operators = {},
+        },
+        flavour = "mocha", -- latte, frappe, macchiato, or mocha
+        transparent_background = true,
+        color_overrides = {
+            all = {
+                -- surface0 = "#444444",
+                -- surface1 = "#666666",
+                -- surface2 = "#a3a7bc",
+                -- surface3 = "#a3a7bc",
+            },
+        },
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            harpoon = true,
+            telescope = true,
+        },
     },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      harpoon = true,
-      telescope = true,
-    },
-  },
-  styles = {
-    comments = { "italic" },
-  },
 }
