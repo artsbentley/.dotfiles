642c71e27c74c89144e885a76f1319478378bfca
Arno Arts <arar@arnos-mbp.home>
2023-03-18 23:25:11 +0100

    lsplines added and other changes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- lsp lines
+-- function to toggle "normal" diagnostics or lsp-lines diagnostics.
+local function toggle_diagnostics()
+    local diagnostics_on = require("lsp_lines").toggle()
+    if diagnostics_on then
+        vim.diagnostic.config({
+            virtual_text = false,
+        })
+    else
+        vim.diagnostic.config({
+            virtual_text = { spacing = 4, prefix = "●" },
+        })
+    end
+end
+
+vim.keymap.set("n", "<Leader>ui", toggle_diagnostics, { desc = "Toggle [i]nline diagnostic type" })
+
 -- harpoon
 vim.keymap.set("n", "<leader>'", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Add to Harpoon" })
 vim.keymap.set("n", "<leader>0", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Show Harpoon" })
