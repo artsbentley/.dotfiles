074edfb7b4d25e078db82a7fdf570e2785048c2a
Arno Arts <arar@arnos-mbp.home>
2023-04-02 01:41:30 +0200

    fead: headlines plugin - not working yet

---
+++ .config/nvim/lua/plugins/headlines.lua
@@ -0,0 +1,33 @@
+-- vim.cmd([[highlight Headline1 guibg=#1e2718]])
+-- vim.cmd([[highlight Headline2 guibg=#21262d]])
+-- vim.cmd([[highlight CodeBlock guibg=#1c1c1c]])
+-- vim.cmd([[highlight Dash guibg=#D19A66 gui=bold]])
+
+-- vim.cmd [[highlight Headline1 guibg=#2C6E00 guifg=#58DB01 gui=italic]]
+-- vim.cmd [[highlight Headline2 guibg=#C8DB01 guifg=#2C6E00 gui=italic]]
+-- vim.cmd [[highlight CodeBlock guibg=#07230E guifg=lightyellow]]
+-- vim.cmd [[highlight Quote guifg=#0099EC]]
+--
+-- return {
+--     "lukas-reineke/headlines.nvim",
+--     dependencies = "nvim-treesitter/nvim-treesitter",
+--     config = function()
+--         require("headlines").setup({
+--             markdown = {
+--                 headline_highlights = { "Headline1", "Headline2" },
+--                 fat_headlines = false,
+--                 fat_headline_upper_string = "▃",
+--                 --
+--                 codeblock_highlight = "CodeBlock",
+--                 --
+--                 quote_highlight = "Quote",
+--                 -- quote_string = "┃",
+--                 quote_string = ">> ",
+--                 --
+--                 dash_highlight = "Dash",
+--                 dash_string = "_",
+--             },
+--         })
+--     end,
+-- }
+--
