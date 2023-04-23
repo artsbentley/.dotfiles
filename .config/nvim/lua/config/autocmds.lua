7020e1c318c60a06771d205179adb8eeff3bcccd
artsbentley <artsbentley@gmail.com>
2023-04-23 11:12:27 +0200

    feat: close  untouched buffers autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,16 +3,50 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*tmux.conf" },
-  command = "execute 'silent !tmux source <afile> --silent'",
+    pattern = { "*tmux.conf" },
+    command = "execute 'silent !tmux source <afile> --silent'",
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { ".yabairc" },
-  command = "!brew services restart yabai",
+    pattern = { ".yabairc" },
+    command = "!brew services restart yabai",
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { ".skhdrc" },
-  command = "!brew services restart skhd",
+    pattern = { ".skhdrc" },
+    command = "!brew services restart skhd",
 })
+
+-- script to close all buffers that havent been "touched" source: https://www.reddit.com/r/neovim/comments/12c4ad8/closing_unused_buffers/
+local id = vim.api.nvim_create_augroup("startup", {
+    clear = false,
+})
+
+local persistbuffer = function(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    vim.fn.setbufvar(bufnr, "bufpersist", 1)
+end
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+    group = id,
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
+            buffer = 0,
+            once = true,
+            callback = function()
+                persistbuffer()
+            end,
+        })
+    end,
+})
+
+vim.keymap.set("n", "<Leader>bc", function()
+    local curbufnr = vim.api.nvim_get_current_buf()
+    local buflist = vim.api.nvim_list_bufs()
+    for _, bufnr in ipairs(buflist) do
+        if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, "bufpersist") ~= 1) then
+            vim.cmd("bd " .. tostring(bufnr))
+        end
+    end
+end, { silent = true, desc = "Close unused buffers" })
