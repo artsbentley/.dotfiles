bab3699cc7c45fc448f70929274885cf1ae4f6b4
Arno Arts <arar@arnos-mbp.home>
2023-08-10 20:05:35 +0200

    feat: mini-surround keymaps

---
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,15 @@
+return {
+    "echasnovski/mini.surround",
+    opts = {
+        mappings = {
+            -- add = "gsa",
+            add = "<Enter>",
+            delete = "<Backspace>",
+            find = "gsf",
+            find_left = "gsF",
+            highlight = "gsh",
+            replace = "gsr",
+            update_n_lines = "gsn",
+        },
+    },
+}
