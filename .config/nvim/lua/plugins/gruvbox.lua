e9d66644174beb0d65f2d14a98cbaabf37849976
artsbentley <artsbentley@gmail.com>
2023-08-28 13:26:05 +0200

    feat(gruvbox): added gruvbox config

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,21 @@
+-- return {}
+return {
+    -- add gruvbox
+    { "sainnhe/gruvbox-material" },
+    -- priority = 1000,
+    -- config = function()
+    -- require("gruvbox").setup({
+    --     palette_overrides = {
+    --         bright_green = "#52a260",
+    --     },
+    -- })
+    -- end,
+
+    -- Configure LazyVim to load gruvbox
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         colorscheme = "gruvbox",
+    --     },
+    -- },
+}
